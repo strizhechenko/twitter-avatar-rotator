8f1aca6a026d2f53637199b36b950e2a9ec587e8
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-11-27 14:10:52 +0500

    Пореже меняю userpic

--- update_pic.py
+++ update_pic.py
@@ -9,7 +9,7 @@ api = Twibot().api
 images = filter(lambda f: 'png' in f or 'jpg' in f, listdir('images/'))
 
 
-@sched.scheduled_job('interval', minutes=15)
+@sched.scheduled_job('interval', hours=20)
 def update_image():
     image = images[randint(0, len(images))]
     api.update_profile_image('images/' + image)
